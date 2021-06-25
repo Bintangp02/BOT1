5fb497d5efa07c54030524fbba06b854eb7e2a62
Bintangp02 <80300921+Bintangp02@users.noreply.github.com>
2021-06-25 15:14:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,4 +29,4 @@ apt-get install -y nodejs libwebp ffmpeg wget tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
  
-echo '©2021• Alphabot All dependencies have been installed, please run the command "npm start" to immediately start the script'
+echo '©2021•  By BP BOT/nBINTANGP/nNAYLA/nRIURICHAN20/nSukses Bro Sekarang ketik "npm start"'
